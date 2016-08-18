bea493d5eb725b887733d8670edf8275518c9237
Oliver Stigley <oliverstigley@gmail.com>
2016-08-18 12:45:28 +1200

    controller generated with index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'to_do/index'
+
+  root 'to_do#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
